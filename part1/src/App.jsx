f895bbc28ba8805ada9b6b70f418f974b6864cc8
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-01-30 15:16:58 +0200

    created new const and called it

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,19 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return(
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Riku"/>    
+      <Hello name="julle" />   
+</div>
+  )
+}
 
 export default App
