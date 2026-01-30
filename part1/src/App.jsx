dc37e67eedc264efdc806da1bcfbf0d62fb6659f
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-01-30 15:43:51 +0200

    used objects in app

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -18,17 +18,16 @@ const Hello = (props) => {
 }
 
 const App = () => {
-  const nimi = 'Julli'
-  const ika = 10+17
-
+  const friends = [
+    { name: 'Leevi', age: 4 },
+    { name: 'Venla', age: 10 },
+  ]
 
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Riku" age={10+16}/>
-      <Hello name={nimi} age={ika}/>   
-      <Footer/>
-    </>
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
   )
 }
 
