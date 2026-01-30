dea0dbf80dbd32002abe542516cf951f39c7e3fc
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-01-30 15:40:32 +0200

    added footer and removed extra div

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,18 +1,34 @@
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by 
+      <a href="https://github.com/Julle999"> Julle999</a>
+    </div>
+  )
+}
+
+
 const Hello = (props) => {
+  console.log(props)
   return(
     <div>
-      <p>Hello {props.name}</p>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
     </div>
   )
 }
 
 const App = () => {
+  const nimi = 'Julli'
+  const ika = 10+17
+
+
   return (
-    <div>
+    <>
       <h1>Greetings</h1>
-      <Hello name="Riku"/>    
-      <Hello name="julle" />   
-</div>
+      <Hello name="Riku" age={10+16}/>
+      <Hello name={nimi} age={ika}/>   
+      <Footer/>
+    </>
   )
 }
 
