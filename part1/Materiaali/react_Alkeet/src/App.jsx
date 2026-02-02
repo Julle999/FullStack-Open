2756f8ee118bb3884beca8756dc99bdaec787457
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-02 15:18:17 +0200

    ReactAlkeet lisätty Counter- ja nollaus-napit

--- part1/Materiaali/react_Alkeet/src/App.jsx
+++ part1/Materiaali/react_Alkeet/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Footer = () => {
   return (
     <div>
@@ -9,7 +11,7 @@ const Footer = () => {
 
 
 const Hello = (props) => {
-  console.log(props)
+  {/*console.log(props)*/}
   const brnyear = () => {
     const yearNow = new Date().getFullYear()
     return yearNow - props.age
@@ -22,8 +24,21 @@ const Hello = (props) => {
   )
 }
 
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  const [counter, setCounter] = useState(50)
+
+  const handleClick = () => {
+    console.log('clicked')
+    //setCounter(counter+1)
+  }
+
+  {/*setTimeout(
+    () => setCounter(counter + 1),
+    20000
+  )*/}
+
+  //console.log('rendering...', counter)
+
   const nimi = 'Pekka'
   const ika = 10
 
@@ -33,6 +48,12 @@ const App = (props) => {
       <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
       <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+            <button onClick={() => setCounter(0)}> 
+        zero
+      </button>
     </div>
   )
 }
