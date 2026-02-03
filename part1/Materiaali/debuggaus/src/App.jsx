ad3576e8d089b4217a6ea92b7c262d82bd41ceac
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-03 13:57:53 +0200

    History ja Button komponentit luotu

--- part1/Materiaali/debuggaus/src/App.jsx
+++ part1/Materiaali/debuggaus/src/App.jsx
@@ -1,5 +1,23 @@
 import { useState } from 'react'
 
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the map is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {allClicks.join(' ')} | {allClicks.length}
+    </div>
+  )
+} 
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
 const App = () => {
   //const [clicks, setClicks] = useState({ left: 0, right: 0 })
   const [left, setLeft] = useState(0)
@@ -21,16 +39,16 @@ const handleRightClick = () =>{
   setTotal(left + updatedRight)
 }
 
-
   return (
     <div>
       <div>
         {left}
-        <button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>
+        <Button onClick={handleLeftClick} text='left'/>
+        <Button onClick={handleRightClick} text='right'/>
+        {/*<button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>*/}
         {right}
-        <p>{allClicks.join(' ')}</p>
-        <p>total {total}</p>
+        <History allClicks={allClicks}/>
       </div>
     </div>
   )
