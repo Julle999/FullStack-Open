91b54e37f9d51ac96aef591c154212d4ee6e6f4f
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-03 14:11:27 +0200

    Funktioita palauttava funktio (Hello(who))

--- part1/Materiaali/debuggaus/src/App.jsx
+++ part1/Materiaali/debuggaus/src/App.jsx
@@ -19,37 +19,48 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 
 const App = () => {
+  const [value, setValue] = useState(10)
+
+  const hello = (who) => () => {
+      console.log('hello', who)
+    }
+  
+
   //const [clicks, setClicks] = useState({ left: 0, right: 0 })
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  //const [left, setLeft] = useState(0)
+  //const [right, setRight] = useState(0)
+  //const [allClicks, setAll] = useState([])
+  //const [total, setTotal] = useState(0)
 
-const handleLeftClick = () => {
-  setAll(allClicks.concat('L'))
-  const updatedLeft = left + 1
-  setLeft(updatedLeft)
-  setTotal(updatedLeft + right)
-}
-
-const handleRightClick = () =>{
-  setAll(allClicks.concat('R'))
-  const updatedRight = right + 1
-  setRight(updatedRight)
-  setTotal(left + updatedRight)
-}
+//const handleLeftClick = () => {
+//  setAll(allClicks.concat('L'))
+//  const updatedLeft = left + 1
+//  setLeft(updatedLeft)
+//  setTotal(updatedLeft + right)
+//}
+//
+//const handleRightClick = () =>{
+//  setAll(allClicks.concat('R'))
+//  const updatedRight = right + 1
+//  setRight(updatedRight)
+//  setTotal(left + updatedRight)
+//}
 
   return (
     <div>
-      <div>
+      {value}
+      <button onClick={hello('world')}>button</button>
+      <button onClick={hello('react')}>button</button>
+      <button onClick={hello('joku')}>button</button>
+      {/*<div>
         {left}
         <Button onClick={handleLeftClick} text='left'/>
         <Button onClick={handleRightClick} text='right'/>
-        {/*<button onClick={handleLeftClick}>left</button>
-        <button onClick={handleRightClick}>right</button>*/}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
         {right}
         <History allClicks={allClicks}/>
-      </div>
+      </div>*/}
     </div>
   )
 }
