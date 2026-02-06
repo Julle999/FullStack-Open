74295860008985eb4c1bf9cf3b65b392bfba7c79
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-06 09:27:08 +0200

    ex.1.7 ready

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,19 +3,23 @@ import { useState } from 'react'
 const Header = ({ text }) => <h1>{text}</h1>
 
 const Button = ({ onClick, text}) => {
-  console.log("button", {text}, "pressed")
+  //console.log("button", {text}, "pressed")
   return (
     <button onClick={onClick}>{text}</button>
   )
 }
 
-const Statistic = ({ value }) => <div>{value}</div>
+const Statistic = ({ value, text }) => <div>{text} {value}</div>
+
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const votes = bad+good+neutral
+  const goods = (good*1)+(bad*-1)
+  const avrg = goods/votes
 
   const addGood = () => {
     console.log("adding good")
@@ -39,9 +43,12 @@ const App = () => {
       <Button onClick={addNeutral} text="neutral"/>
       <Button onClick={addBad} text="bad"/>
       <Header text="statistics"/>
-      <Statistic value={good}/>
-      <Statistic value={neutral}/>
-      <Statistic value={bad}/>
+      <Statistic value={good} text="good"/>
+      <Statistic value={neutral} text="neutral"/>
+      <Statistic value={bad} text="bad"/>
+      <Statistic value={votes} text="all"/>
+      <Statistic value={avrg} text="average"/>
+      <Statistic value={good/votes+ "%"} text="positive"/>
     </div>
   )
 }
