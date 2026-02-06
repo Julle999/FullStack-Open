6c79196335fefc5ca4c8c2a8173f988b39442917
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-06 12:10:07 +0200

    ex.1.8 ready

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,15 +11,34 @@ const Button = ({ onClick, text}) => {
 
 const Statistic = ({ value, text }) => <div>{text} {value}</div>
 
+const Statistics = ({stats}) => {
+  const votes = stats[0]+stats[1]+stats[2]
+  const goods = (stats[0]*1)+(stats[2]*-1)
+  const avrg = goods/votes
+
+  return (
+    <div>
+      <Statistic value={stats[0]} text="good"/>
+      <Statistic value={stats[1]} text="neutral"/>
+      <Statistic value={stats[2]} text="bad"/>
+      <Statistic value={votes} text="all"/>
+      <Statistic value={avrg} text="average"/>
+      <Statistic value={stats[0]/votes+ "%"} text="positive"/>
+    </div>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const votes = bad+good+neutral
-  const goods = (good*1)+(bad*-1)
-  const avrg = goods/votes
+  
+  const stats = [good, neutral, bad]
+
+  //const votes = bad+good+neutral
+  //const goods = (good*1)+(bad*-1)
+  //const avrg = goods/votes
 
   const addGood = () => {
     console.log("adding good")
@@ -43,12 +62,13 @@ const App = () => {
       <Button onClick={addNeutral} text="neutral"/>
       <Button onClick={addBad} text="bad"/>
       <Header text="statistics"/>
-      <Statistic value={good} text="good"/>
+      <Statistics stats={stats}/>
+      {/*<Statistic value={good} text="good"/>
       <Statistic value={neutral} text="neutral"/>
       <Statistic value={bad} text="bad"/>
       <Statistic value={votes} text="all"/>
       <Statistic value={avrg} text="average"/>
-      <Statistic value={good/votes+ "%"} text="positive"/>
+      <Statistic value={good/votes+ "%"} text="positive"/>*/}
     </div>
   )
 }
