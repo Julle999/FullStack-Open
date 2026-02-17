70bd16f9ea484bd8483c8599b2b1fb42b28590bb
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-17 08:20:05 +0200

    phonebook step 3 Done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number:'040123456'}]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
-  const addName = (event) => {
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const addPerson = (event) => {
     event.preventDefault()
     console.log('Button clicked', event.target)
     const personObject = {
-      name : newName
+      name : newName,
+      number : newNumber
     }
     const names = persons.map((person) => person.name)
 
@@ -22,29 +29,26 @@ const App = () => {
 
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     } else {
       window.alert(`${newName} is already added to phonebook`)
       //setNewName('')
     }
   } 
 
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName}
-                       onChange={handleNameChange}
-                />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input value={newName}onChange={handleNameChange}/></div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       
         {persons.map(person =>
-          <Person key={person.name} name={person.name}/>
+          <Person key={person.name} name={person.name} number={person.number}/>
 
         )}
       
