2f00e885ae0a7054fb45b79cf769625b8813253e
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-02-24 13:32:17 +0200

    PB-Bend step 7-8 DONE

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-
-app.use(express.json())
 
 let persons = [
     {
@@ -25,6 +24,10 @@ let persons = [
       "id": "4"
     }
   ]
+
+morgan.token('body', (req,res) => JSON.stringify(req.body))
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
