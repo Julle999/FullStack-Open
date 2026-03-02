04c4b9148ebd06f275cf59a94de81453fc833c28
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-03-02 09:56:59 +0200

    uibuild

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const app = express()
 
@@ -30,7 +29,6 @@ let persons = [
 morgan.token('body', (req,res) => JSON.stringify(req.body))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(cors())
 app.use(express.static('dist'))
 
 app.get('/api/persons/:id', (request, response) => {
