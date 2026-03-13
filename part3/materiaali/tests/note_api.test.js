164a71b62751f54d978d997367842974534e12f2
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-03-13 13:47:40 +0200

    Materiaali käyttäjien luominen asti

--- part3/materiaali/tests/note_api.test.js
+++ part3/materiaali/tests/note_api.test.js
@@ -1,16 +1,71 @@
-const {test, after, beforeEach} = require('node:test')
+const {test, after, beforeEach, describe} = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
 const Note = require('../models/note')
 const helper = require('./test_helper')
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 
 const api = supertest(app)
 
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+  })
+})
 
 
-beforeEach(async () => {
+/*beforeEach(async () => {
   await Note.deleteMany({})
   //console.log('deleted many')
   await Note.insertMany(helper.initialNotes)
@@ -113,7 +168,7 @@ test('a note can be deleted', async () => {
   assert(!ids.includes(noteToDelete.id))
 
   assert.strictEqual(notesAtEnd.length, helper.initialNotes.length -1)
-})
+})*/
 
 after(async () => {
   await mongoose.connection.close()
--- part3/materiaali/utils/config.js
+++ part3/materiaali/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI_NOTEAPP = process.env.NODE.ENV === "test"
+const MONGODB_URI_NOTEAPP = process.env.NODE_ENV === "test"
     ? process.env.TEST_MONGODB_URI_NOTEAPP
     : process.env.MONGODB_URI_NOTEAPP
 
