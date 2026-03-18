168aa347fe0f7d02fa94242d055fdb5a7b9f6ddb
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-03-18 13:23:17 +0200

    ex 4.18: blogilistan laajennus, step6 DONE

--- part4/blogilista/app.js
+++ part4/blogilista/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const morgan = require('morgan')
 
 
@@ -23,6 +24,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 
---
+++ part4/blogilista/controllers/login.js
@@ -0,0 +1,40 @@
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+loginRouter.post('/', async (request, response) => {
+    const { username, password } = request.body
+
+    const user = await User.findOne({ username })
+
+    console.log(user.id, 'id')
+    console.log(user._id, '_id')
+
+    const passwordCorrect = bcrypt.compare(password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return esponse.status(401).json({
+            error: 'invalid username or password'
+        })
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+    
+    response
+        .status(200)
+        .send({ token, username: user.username, name: user.name })
+})
+
+loginRouter.get('/', async (request, response) => {
+    const olio = {sivu: 'löytyy'}
+    response.status(200).json(olio)
+})
+
+
+module.exports = loginRouter
