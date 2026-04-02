204d200a5882b76d1877eac9ce22eb78d8f26950
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-04-02 13:05:33 +0300

    ex 6.3-6.8: anekdootit, step1-6 DONE

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,21 +3,12 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import noteReducer from './reducers/noteReducer'
+import reducer from './reducers/anecdoteReducer'
 
-const store = createStore(noteReducer)
+const store = createStore(reducer)
 
-
-//const root = 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>
 )
-
-//const renderApp = () => {
-//  root.render(<App />)
-//}
-//
-//renderApp()
-//store.subscribe(renderApp)
