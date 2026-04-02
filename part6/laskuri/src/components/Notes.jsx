28c30561adb35a573f9e9179c38e4260373f9824
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-04-02 15:36:49 +0300

    materiaali: Monta reduseria - Filtteröinnin viimeistely

--- part6/laskuri/src/components/Notes.jsx
+++ part6/laskuri/src/components/Notes.jsx
@@ -3,28 +3,37 @@ import { toggleImportanceOf } from "../reducers/noteReducer"
 
 
 const Note = ({ note, handleClick }) => {
-    return (
-        <li onClick={handleClick}>
-            {note.content} 
-            <strong>{note.important ? 'important' : ''}</strong>
-        </li>
-    )
+  return (
+    <li onClick={handleClick}>
+      {note.content} 
+      <strong>{note.important ? 'important' : ''}</strong>
+    </li>
+  )
 }
 
 const Notes = () => {
-    const dispatch = useDispatch()
-    const notes = useSelector(state => state)
-    return (
-        <ul>
-        {notes.map(note => (
+  const dispatch = useDispatch()
+
+  const notes = useSelector(({ filter, notes }) => {
+    if (filter === 'ALL') {
+      return notes
+    }
+    return filter === 'IMPORTANT'
+      ? notes.filter(note => note.important)
+      : notes.filter(note => !note.important)
+  })
+
+  return (
+    <ul>
+      {notes.map(note => (
         <Note
             key={note.id}
             note={note}
             handleClick={() => dispatch(toggleImportanceOf(note.id))}
         />  
-        ))}
-      </ul>
-    )
+      ))}
+    </ul>
+  )
 }
 
-export default { Notes }
+export default Notes
--- part6/laskuri/src/main.jsx
+++ part6/laskuri/src/main.jsx
@@ -1,23 +1,22 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { createStore, combineReducers } from 'redux'
 
 import App from './App'
+import filterReducer from './reducers/filterReducer'
 import noteReducer from './reducers/noteReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
 
+const store = createStore(reducer)
 
-//const root = 
+console.log(store.getState())
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App/>
+    <App />
   </Provider>
 )
-
-//const renderApp = () => {
-//  root.render(<App />)
-//}
-//
-//renderApp()
-//store.subscribe(renderApp)
