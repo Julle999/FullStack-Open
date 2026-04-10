c0a795a3f168f7ddc549bc6aea5e374aaabb12c8
Julius Koskelo <julius.g.koskelo@gmail.com>
2026-04-10 14:03:25 +0300

    Materiaali -> laskurikontekstin määrittely omassa tiedostossa

---
+++ part6/context-counter/src/App.jsx
@@ -0,0 +1,17 @@
+import Panel from './components/Panel'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import CounterContext from './CounterContext'
+
+const App = () => {
+ 
+  return (
+    <div>
+      <Navbar />
+      <Panel/>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
